8887135b78bbace6ab3864a54bad93889f063d58
Rick <ricksmycodeguy@gmail.com>
2021-09-28 16:49:54 -0500

    Add [GET] /api/posts/:id, tests 3-4 passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -7,8 +7,8 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        const data = await Post.find();
-        res.status(200).json(data);
+        const posts = await Post.find();
+        res.status(200).json(posts);
     } catch (err) {
         res.status(500).json({
             message: "The posts information could not be retrieved",
@@ -16,4 +16,20 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (post) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post information could not be retrieved"
+        })
+    }
+})
 module.exports = router;
