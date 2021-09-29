a96d5b6ae7e85d094952789e33599cf04b8e7d8d
Rick <ricksmycodeguy@gmail.com>
2021-09-28 20:52:56 -0500

    add [PUT] /api/posts/:id tests 1-11 passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -59,31 +59,52 @@ router.post('/', async (req, res) => {
     }
 })
 
-// router.put('/:id', async (req, res) => {
-//     try {
-//         const { title, contents } = req.body
-//         const { id } = req.params
-//         if(!title || !contents) {
-//             res.status(400).json({
-//                 message: "Please provide title and contents for the post"
-//             })
-//         } else {
-//             const editPost = await Post.editPost(id, { title, contents })
-//             if (editPost)
-//         }
-//     } catch (err) {
-//         res.status(500).json({
+router.put('/:id', async (req, res) => {
+    try {
+        const { title, contents } = req.body
+        const { id } = req.params
+        if (! await Post.findById(id)){
+            return res.status(404).json({message: "The post with the specified ID does not exist"})
+        }
+        if(!title || !contents) {
+            res.status(400).json({
+                message: "Please provide title and contents for the post"
+            })
+        } else {
+            const editPost = await Post.update(id, { title, contents })
+            console.log('XXXXXXXX',editPost);
+            // if (editPost === 0) {
+            //     res.status(404).json({ message: "The post with the specified ID does not exist" })
+            // } else {
+            res.status(200).json({
+                title, 
+                contents,
+                id: Number(id)
+            })
+            // }
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post information could not be modified"
+        })
+    }
+})
 
-//         })
-//     }
-// })
-// router.verb('/', async (req, res) => {
-//     try {} catch (err) {
-//         res.status(500).json({
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletePost = await Post.remove(req.params.id)
+        if (!deletePost) {
+            res.status(404).json({ 
+                message: "The post with the specified ID does not exist" 
+            })
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post could not be removed"
+        })
+    }
+})
 
-//         })
-//     }
-// })
 // router.verb('/', async (req, res) => {
 //     try {} catch (err) {
 //         res.status(500).json({
